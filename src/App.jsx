1b1978f139c898a7c53c8061d8f9d7643e071d24
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-03-29 01:18:14 +0200

    practice JSX and component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
+const elements = [
+  { id: 1, product: 'laptop' },
+  { id: 2, product: 'mobile' },
+  { id: 3, product: 'tab' },
+  { id: 4, product: 'monitor' },
+  { id: 5, product: 'mouse' },
+  { id: 6, product: 'keyboard' },
+];
+
 function App() {
+  // jsx code
   return (
     <div className="App">
-      <h1>Hello</h1>
+      {elements.map((element) => (
+        <MyComponent element={element}></MyComponent>
+        // <MyComponent product={element.product} id={element.id}></MyComponent>
+      ))}
     </div>
   );
 }
+
+const MyComponent = (props) => {
+  const { id, product } = props.element;
+  // const MyComponent = ({ id, product }) => {
+  return (
+    <div>
+      <div style={{ display: 'flex', columnGap: '15px' }}>
+        <p>Product ID : {id}</p>
+        <p>Product Name: {product}</p>
+      </div>
+    </div>
+  );
+};
 
 export default App;
